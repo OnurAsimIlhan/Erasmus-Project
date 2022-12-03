d703f747daf1e6aeb772a9b8b91b11e957e8e18e
MuratGuneyKemal <muratguneykemal@gmail.com>
2022-12-03 23:18:59 +0300

    syllabus download procedure is created

--- website/controllers/course_coordinator_controller.py
+++ website/controllers/course_coordinator_controller.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, send_from_directory, send_file
 from flask_login import login_required, current_user, logout_user
 from flask.views import MethodView
-
-
+import os
+import webbrowser
 class CourseCoordinatorController(MethodView):
     def __init__(self, auth_service):
         self.auth_service = auth_service
@@ -15,4 +15,16 @@ class CourseCoordinatorController(MethodView):
         else:
             logout_user() 
             return redirect(url_for("your_are_not_authorized_page"))  
+    
+    @login_required
+    def post(self):
+        if self.auth_service.is_authorized(user=current_user, required_role="Course Coordinator"):
+            course_id = request.form.get('evaluate')
+            # pathi standart yapmak lazım
+            return send_file("C:\\Users\\Murat\\Documents\\GitHub\\Erasmus-Project\\Syllabus\\"+ course_id+".pdf", as_attachment=True)
+        else:
+            logout_user() 
+            return redirect(url_for("your_are_not_authorized_page"))  
+             
+            
         
