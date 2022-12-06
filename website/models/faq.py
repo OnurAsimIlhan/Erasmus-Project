133acf9a5c913508d487f22c68d208efa7129002
dem0-0 <emirkan.derkoken1@gmail.com>
2022-12-06 18:03:32 +0300

    merge

--- website/models/faq.py
+++ website/models/faq.py
@@ -6,4 +6,4 @@ class Faq(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     department = db.Column(db.String(5), nullable=False)
     info = db.Column(db.String(50), nullable=False)
-    date = db.Column(db.DateTime)
+    #date = db.Column(db.DateTime)
