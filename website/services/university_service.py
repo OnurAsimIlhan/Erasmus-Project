d2c40f6e8ccd859f738e93e182b1c728f73e4e9b
Lorekkan <82536623+Lorekkan@users.noreply.github.com>
2022-12-15 14:54:59 +0300

    Updated universities page according to new database model

--- website/services/university_service.py
+++ website/services/university_service.py
@@ -13,23 +13,28 @@ class UniversityService():
         university = self.university_table.query.filter_by(university_id = id).first()
         return university
 
-    def getUniversitiesByDepartment(self, department):
-        all_data = self.university_table.query.filter_by(departments = department).all()
-        return all_data
-
     def addUniversity(self, name: str, country: str, semester: str, department: str, language: str, quota: int):
         new_university = self.university_table(
             name = name,
             country = country,
             semester = semester,
-            departments = department,
             language_requirements = language,
             total_quota = quota,
             remaining_quota = quota
         )
         db.session.add(new_university)
         db.session.commit()
+        self.addDepartment(department = department, university_id = new_university.university_id)
         return new_university
+
+    def addDepartment(self, department: str, university_id: int):
+        new_department = self.university_departments_table(
+            department = department,
+            university_id = university_id
+        )
+        db.session.add(new_department)
+        db.session.commit()
+        return new_department
 
     def updateUniversity(self, id: int, name: str, country: str, semester: str, language: str, total_quota: int, remaining_quota: int):
         university = self.getUniversityById(id)
@@ -49,4 +54,6 @@ class UniversityService():
         universities = [self.university_table.query.filter_by(university_id=id.university_id).first() for id in university_ids]
             
         return universities
-   
+
+    def getUniversityByName(self, name: str):
+        return self.university_table.query.filter_by(name=name).first()
