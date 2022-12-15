106c816d9e4dbd84f8e26cafbc5d703561cc7fd6
dem0-0 <emirkan.derkoken1@gmail.com>
2022-12-15 19:45:49 +0300

    merge

--- website/controllers/student_home_controller.py
+++ website/controllers/student_home_controller.py
@@ -8,9 +8,9 @@ from website.services import AuthorizeService
 class StudentHome(MethodView, AuthorizeService):
     decorators = [login_required]
 
-    def __init__(self, role: str, applications_service):
+    def __init__(self, role: str, deadline_service, applications_service):
         AuthorizeService.__init__(self, role=role)
-        
+        self.deadline_service = deadline_service
         self.applications_service = applications_service
 
     def get(self):
@@ -18,7 +18,12 @@ class StudentHome(MethodView, AuthorizeService):
             logout_user()
             return redirect(url_for("your_are_not_authorized_page"))
 
-        
+        application_deadline_bool = self.deadline_service.has_passed("application_deadline")
+        preapproval_deadline_bool = self.deadline_service.has_passed("preapproval_deadline")
+        learning_agreement_deadline_bool = self.deadline_service.has_passed(
+            "learning_agreement_deadline"
+        )
+
         matched_university = self.applications_service.getApplicationByStudentId(
             current_user.bilkent_id
         ).matched_university
@@ -28,6 +33,8 @@ class StudentHome(MethodView, AuthorizeService):
         return render_template(
             "student_homepage.html",
             name=current_user.name,
-            
+            application_deadline=application_deadline_bool,
+            preapproval_deadline=preapproval_deadline_bool,
+            learning_agreement_deadline=learning_agreement_deadline_bool,
             matched_university=matched_university
         )
