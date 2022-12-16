9003cd28b111fdaccc35edf4f738ffe3692f4718
Lorekkan <82536623+Lorekkan@users.noreply.github.com>
2022-12-16 23:46:00 +0300

    view applications and application period are removed; navbars of erasmus coordinator reorganized

--- website/models/applications.py
+++ website/models/applications.py
@@ -23,8 +23,6 @@ class Applications(db.Model):
     pre_approval_form = db.Column(db.LargeBinary)
     learning_agreement_form = db.Column(db.LargeBinary)
     final_transfer_form = db.Column(db.LargeBinary)
-        
-    application_period_id = db.Column(db.Integer, db.ForeignKey("application_period.id"), nullable=True)
     
     def get_id(self):
         return self.application_id
